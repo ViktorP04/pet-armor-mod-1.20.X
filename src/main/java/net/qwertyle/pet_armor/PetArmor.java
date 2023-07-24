a7c0d288b5376acbc29e74764a8c9927dec2f69f
ViktorP04 <viktor@pennskog.com>
2023-07-25 00:13:33 +0200

    Initial commit

---
+++ src/main/java/net/qwertyle/pet_armor/PetArmor.java
@@ -0,0 +1,19 @@
+package net.qwertyle.pet_armor;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PetArmor implements ModInitializer {
+
+	public static final String MOD_ID = "pet-armor";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/qwertyle/pet_armor/PetArmorClient.java
@@ -0,0 +1,9 @@
+package net.qwertyle.pet_armor;
+
+import net.fabricmc.api.ClientModInitializer;
+public class PetArmorClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
